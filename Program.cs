0673ef2eaafe19e29934ab3438a90a7d52ee80ef
crisb <cfbarbero@gmail.com>
2017-04-10 14:53:03 -0600

    commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var app = new Microsoft.Extensions.CommandLineUtils.CommandLineApplication();
+            app.Command("catapult", config => { });
+            app.Command("snowball", config => { });
+            //give people help with --help
+            app.HelpOption("-? | -h | --help");
+            app.Execute(args);
         }
     }
 }
